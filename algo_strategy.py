5082616c718928eef75283ad3c8bfc054250b52d
yuhaowang2018 <43505575+yuhaowang2018@users.noreply.github.com>
2019-09-14 16:09:57 -0500

    update defense

--- algo_strategy.py
+++ algo_strategy.py
@@ -176,7 +176,7 @@ class AlgoStrategy(gamelib.AlgoCore):
                 if i==len(dlocations2):
                     break
                 if game_state.contains_stationary_unit(dlocations2[i]):
-                    for unit in game_state.game_map[dlocations2[i]]
+                    for unit in game_state.game_map[dlocations2[i]]:
                         if unit.unit_type == ENCRYPTOR:
                             game_state.attempt_remove(dlocations2[i])
                     
